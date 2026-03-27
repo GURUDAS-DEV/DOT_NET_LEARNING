a82f4c63189e51139ef4f62f6414b369f5f22413
Gurudas Bhardwaj <gursad5@gmail.com>
2026-03-27 23:49:57 +0530

    Update AssemblyInfo with new informational version and cache input

--- obj/Debug/net10.0/FirstProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d473624b841f1df2831a8f33c513065143a1abc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df3224a68fd902c4d5dd62ae593a151b2e1ecad")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
